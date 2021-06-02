76658c7f00195b2fb1e0d098cf6fc2783c0280fa
phamtienanh157 <phamtienanh157@gmail.com>
2021-06-02 20:44:31 +0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "font-awesome/css/font-awesome.css";
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
